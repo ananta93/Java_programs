8cf6ad03c06950e4efb845f3cd99242debbbafc7
Ananta Chandra Das <anantadas.dilan@gmail.com>
2016-07-11 23:06:30 +0530

    Create CommandLineTest.java

---
+++ CommandLineTest.java
@@ -0,0 +1,9 @@
+//Write a program in java to take input from through the command prompt and display it
+class CommandLineTest 
+{
+	public static void main(String[] args) 
+	{
+		String input=args[0];
+		System.out.println("Your input is : "+input);
+	}
+}
